269a18ee1b1b506ece7bd00d3fdbb4ef3abb196c
theines <churn8282@naver.com>
2021-10-21 15:00:29 +0900

    recap done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data:
+      { data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => {
+          return (
+            <Movie 
+              key={movie.id}
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image} 
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
+
+/*
+const options = {
+  method: 'GET',
+  url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+  params: {symbol: 'AMRN', region: 'US'},
+  headers: {
+    'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+    'x-rapidapi-key': '04f36f0fa1msh968f496b4b8068dp162ad2jsn02864c1da27a'
+  }
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+*/
 
 export default App;
